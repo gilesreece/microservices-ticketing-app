a1d6e42f683c0be53f9e151270e654401760d699
Reece Giles <reeceyy@me.com>
2022-02-03 00:09:09 +0000

    224 hooks

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,21 +1,23 @@
 import {useState} from "react";
-import axios from "axios";
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
 const signupAuth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: () => Router.push('/')
+    });
 
-    const onSumbit = async (event) => {
+    const onSumbit = (event) => {
         event.preventDefault();
 
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            });
-        } catch (err) {
-            setErrors(err.response.data.errors);
-        }
+        doRequest();
     }
 
     return (
@@ -31,12 +33,7 @@ const signupAuth = () => {
                        onChange={e => setPassword(e.target.value)}/>
             </div>
             <button className="btn btn-primary">Sign Up</button>
-            {errors.length > 0 && <div className="alert alert-danger">
-                <h4>Ooops...</h4>
-                <ul className="my-0">
-                    {errors.map(err => <li key={err.message}>{err.message}</li>)}
-                </ul>
-            </div>}
+            {errors}
         </form>
     );
 };
