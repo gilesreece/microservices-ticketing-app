e5648be6b092ee6858f541d6f43b61c293410ce0
Reece Giles <dev@reecegiles.com>
2022-05-09 23:38:31 +0100

    Added first publisher using TS

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from "express";
 import {body} from 'express-validator';
 import {requireAuth, validateRequest} from "@gilesreece2/common";
 import {Ticket} from "../models/ticket.model";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -19,8 +21,14 @@ router.post(
             price,
             userId: req.currentUser!.id
         });
-
         await ticket.save();
+
+        await new TicketCreatedPublisher(natsWrapper.client).publish({
+            id: ticket.id,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId
+        });
 
         res.status(201).send(ticket);
     });
